b3001be7906fdfcb05f90a4f3adb7d552ba6a306
Daniela-712 <daniela.camargo@alumnos.udg.mx>
2025-04-25 14:15:52 -0600

    Delete usuario.py

--- usuario.py
+++
@@ -1,9 +0,0 @@
-class Usuario:
-    def __init__(self, usuarios):
-        self.usuarios = usuarios
-
-    def validar(self, nombre, contraseña):
-        user = self.usuarios.get(nombre)
-        if user and user["password"] == contraseña:
-            return user["rol"]
-        return None
